c33e6a8acaec0e2d620f097da67161593cd18f30
Ville <Ville.lauttajarvi@gmail.com>
2018-01-24 00:01:25 +0200

    Method toString()

---
+++ Circle/CircleClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * The Circle class models a circle with a radius and color.
+ */
+public class Circle {  // Save as "Circle.java"
+   // private instance variable, not accessible from outside this class
+   private double radius;
+   private String color;
+   
+   // The default constructor with no argument.
+   // It sets the radius and color to their default value.
+   public Circle() {
+      radius = 1.0;
+      color = "red";
+   }
+   
+   // 2nd constructor with given radius, but color default
+   public Circle(double radius) {
+      this.radius = radius;
+      this.color = "red";
+   }
+    public Circle(double radius, string color)
+    {
+        this.radius = radius;
+        this.color = "red";
+    }
+   
+   // A public method for retrieving the radius
+   public double getRadius() {
+     return this.radius; 
+   }
+   
+   // A public method for computing the area of circle
+   public double getArea() {
+      return this.radius*this.radius*Math.PI;
+   }
+
+    public String getColor()
+    {
+        return color;
+    }
+    // Setter for instance variable radius
+    public void setRadius(double newRadius)
+    {
+        this.radius = newRadius;
+    }
+
+    // Setter for instance variable color
+    public void setColor(String newColor)
+    {
+        this.color = newColor;
+    }
+    // Return a description of this instance in the form of
+    // Circle[radius=r,color=c]
+    public override String ToString()
+    {
+        return "Circle[radius=" + radius + " color=" + color + "]";
+    }
+}
---
+++ Circle/TestCircle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class TestCircle
+{  // Save as "TestCircle.java"
+    public static void Main(String[] args)
+    {
+        // Declare an instance of Circle class called c1.
+        // Construct the instance c1 by invoking the "default" constructor
+        // which sets its radius and color to their default value.
+        Circle c1 = new Circle();
+        // Invoke public methods on instance c1, via dot operator.
+        Console.WriteLine("The circle has radius of "
+           + c1.getRadius() + " and area of " + c1.getArea());
+
+        // Declare an instance of class circle called c2.
+        // Construct the instance c2 by invoking the second constructor
+        // with the given radius and default color.
+        Circle c2 = new Circle(2.0);
+        // Invoke public methods on instance c2, via dot operator.
+        Console.WriteLine("The circle has radius of "
+           + c2.getRadius() + " and area of " + c2.getArea());
+
+        Circle c3 = new Circle(2.0);
+
+        Console.WriteLine("The circle has radius of "
+            + c2.getRadius() + " and area of " + c2.getArea() + " and color of " + c2.getColor());
+
+        Circle c4 = new Circle();   // construct an instance of Circle
+        c4.setRadius(5.0);          // change radius
+        c4.setColor("blue");        // Change color
+        Console.WriteLine("The circle has radius is: " + c4.getRadius() + // Print radius via getter
+        " and the circle color is: " + c4.getColor());  // Print color via getter
+
+        // You cannot do the following because setRadius() returns void,
+        // which cannot be printed.
+        // Console.WriteLine(c4.setRadius(4.0));
+    }
+} 
